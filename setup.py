af2881ea14a3586a308e709a5ff941fca0d0c455
Diego Trindade de Souza <diegotsouza@hotmail.com>
2022-03-04 21:29:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="FancyParser",
     version="0.0.1",
-    py_modules=["FancyParser", ],
-    install_requires=["pandas"],
+    py_modules=["FancyParser"],
+    install_requires=["pandas"]
 )
