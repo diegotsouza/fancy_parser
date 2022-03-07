2c983c1a0a8e4c4e0b2ea172f024f960b3d0e261
Lucas Ferreira da Silva <LucasSilvaFerreira@users.noreply.github.com>
2022-03-07 12:00:21 -0500

    Update FancyParser.py

--- FancyParser.py
+++ FancyParser.py
@@ -4,16 +4,25 @@ from IPython.display import display, HTML
 class FancyParser():
   def __init__(self, file_list):
     self.files = file_list
-    self.df = pd.DataFrame(self. files, columns=['file_path'])
+    self.df = pd.DataFrame(self. files, columns=['file_path'] )
+    self.__create_name__()
 
-  def add_col_parse(self,  column_to_parse=None, split_string=None, position_split=None, remove_end = ''):
 
+  def __create_name__(self):
+    self.df['file_name'] =  self.add_col_parse( column_to_parse='file_path', split_string='/', position_split=-1, remove_end = '')
+
+
+  def add_col_parse(self,  column_to_parse=None, split_string=None, position_split=None, remove_end = '', new_column=None):
+    
+    if new_column:
+      self.df[new_column] = self.df[column_to_parse].apply(lambda x : x.split(split_string)[position_split].replace(remove_end,''))
     return self.df[column_to_parse].apply(lambda x : x.split(split_string)[position_split].replace(remove_end,''))
 
   
-  def add_flag(self,  column_to_parse=None, check_substr='' , return_type=None):
+  def add_flag(self,  column_to_parse=None, check_substr='' , return_type=None, new_column=None):
 
-
+    if new_column:
+      self.df[new_column] = self.df[column_to_parse].apply(lambda x : return_type[0] if check_substr in x else return_type[1] )
     return self.df[column_to_parse].apply(lambda x : return_type[0] if check_substr in x else return_type[1] )
 
   def __repr__(self):
@@ -30,6 +39,11 @@ class FancyParser():
   def load_from_df(self):
     self.df = pd.read_csv(df_file_to_load ,sep='\t')
     
-  def save_to_df(self, out_path, df_file_name):
-    pd.to_csv(out_path ,sep='\t', index=None)
- 
+  def save_to_df(self, out_path):
+    self.df.to_csv(out_path ,sep='\t', index=None)
+
+  def capture_files(self, grouped_by=None):
+    out_list = []
+    for k,v in self.df.groupby(grouped_by):
+      out_list.append([k,v['file_path'].values.tolist()])
+    return out_list
